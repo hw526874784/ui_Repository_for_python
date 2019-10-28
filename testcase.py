10ee4a70b31a95b6b785de8317dd8afc26a87690
何伟 <hewei@heweis-Mac-mini.local>
2019-10-28 15:45:36 +0800

    上传ui自动化测试用例

---
+++ testcase.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+import unittest
+import os
+# 用例路径
+case_path = os.path.join(os.getcwd(), "Test_case")
+# 报告存放路径
+report_path = os.path.join(os.getcwd(), "report")
+from Resource import myresource
+
+
+myresource.pwdlogin()
+myresource.send_message()
+myresource.send_photo()
+myresource.lock_start()
+myresource.secretly()
+myresource.user_information()
+myresource.location_send()
+myresource.home_search()
